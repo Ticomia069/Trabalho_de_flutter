38430ae146787b6e315290d2ca11c19bc41b5d3e
Tico_Emanoel <152090787+Ticomia069@users.noreply.github.com>
2025-06-03 19:57:55 -0300

    Create habit_tile.dart

---
+++ habit_tile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../models/habit.dart';
+
+class HabitTile extends StatelessWidget {
+  final Habit habit;
+  final Function(bool?) onChanged;
+
+  HabitTile({required this.habit, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(habit.name),
+      trailing: Checkbox(
+        value: habit.completed == 1,
+        onChanged: onChanged,
+      ),
+    );
+  }
+}
